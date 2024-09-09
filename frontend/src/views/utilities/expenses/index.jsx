dd54555253f63f4817e47fa1671ca8c4421814da
anvMitva <mitva.v@anvtech.co>
2024-09-09 10:25:02 +0530

    test 2

--- frontend/src/views/utilities/expenses/index.jsx
+++ frontend/src/views/utilities/expenses/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import ExpenseTable from './components/ExpenseTable';
 
-const Expenses = () => {
-
-  console.log("njiodbasic");
-  
+const Expenses = () => {  
   return (
     <>
       <MainCard title="Expenses">
